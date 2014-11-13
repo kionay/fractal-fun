43e8e10601bb742b8e5e32683cd8b36b8882444a
kionay <kizonay@yahoo.com>
2014-11-13 15:55:51 -0500

    prettier code
    code is prettier thanks to \\z
    
    added attribute on script tags, added clear() button, and changed the
    input tags to make expression web 4 happier
    
    zoom still doesn't work

--- ww.js
+++ ww.js
@@ -21,80 +21,47 @@ self.addEventListener('message', function(e) {
 }, false);
 
 function fractilize(xCoords,yCoords){
-	//find the direction of the line	
+//find the direction of the line
 	var newXcoords = [0,1,2,3,4,5];
 	var newYcoords = [0,1,2,3,4,5];
 	var lineLength;
 	var unitDist;
 	//first and last items are the same
-	newXcoords[0] = xCoords[0];newYcoords[0] = yCoords[0];
-	newXcoords[5] = xCoords[1];newYcoords[5] = yCoords[1];
+	
+	newXcoords[0] = xCoords[0];
+	newYcoords[0] = yCoords[0];
+	
+	newXcoords[5] = xCoords[1];
+	newYcoords[5] = yCoords[1];
 	
 	if(xCoords[0] == xCoords[1]){ //x's are the same, line is vertical
-		if(yCoords[0] > yCoords[1]){
-			//going down
-			lineLength = yCoords[0] - yCoords[1];
-			unitDist = lineLength / 3;
-			
-			newXcoords[1] = xCoords[0];
-			newXcoords[2] = newXcoords[1] + unitDist;
-			newXcoords[3] = newXcoords[2];
-			newXcoords[4] = newXcoords[1];
-			
-			newYcoords[1] = yCoords[0] - unitDist;
-			newYcoords[2] = newYcoords[1];
-			newYcoords[3] = newYcoords[2] - unitDist;
-			newYcoords[4] = newYcoords[3]
-		}
-		else{
-			//going up
-			lineLength = yCoords[1] - yCoords[0];
-			unitDist = lineLength / 3;
-			
-			newXcoords[1] = xCoords[0];
-			newXcoords[2] = newXcoords[1] - unitDist;
-			newXcoords[3] = newXcoords[2];
-			newXcoords[4] = newXcoords[1];
-			
-			newYcoords[1] = yCoords[0] + unitDist;
-			newYcoords[2] = newYcoords[1];
-			newYcoords[3] = newYcoords[2] + unitDist;
-			newYcoords[4] = newYcoords[3];
-		}
+		lineLength = yCoords[0] - yCoords[1];
+		unitDist = lineLength / 3;
+		
+		newXcoords[1] = xCoords[0];
+		newXcoords[2] = xCoords[0] + unitDist;
+		newXcoords[3] = xCoords[0] + unitDist;
+		newXcoords[4] = xCoords[0];
+		
+		newYcoords[1] = yCoords[0] - unitDist;
+		newYcoords[2] = yCoords[0] - unitDist;
+		newYcoords[3] = yCoords[0] - (unitDist*2);
+		newYcoords[4] = yCoords[0] - (unitDist*2);
 	}
 	else{ //y's are the same, line is horizontal
-		if(xCoords[0] > xCoords[1]){
-			//going left
-			lineLength = xCoords[0] - xCoords[1];
-			unitDist = lineLength / 3;
-			
-			newXcoords[1] = xCoords[0] - unitDist;
-			newXcoords[2] = newXcoords[1];
-			newXcoords[3] = newXcoords[2] - unitDist;
-			newXcoords[4] = newXcoords[3];
-			
-			newYcoords[1] = yCoords[0];
-			newYcoords[2] = newYcoords[1] + unitDist;
-			newYcoords[3] = newYcoords[2];
-			newYcoords[4] = newYcoords[1];
-		}
-		else{
-			//going right
-			lineLength = xCoords[1] - xCoords[0];
-			unitDist = lineLength / 3;
-			
-			newXcoords[1] = xCoords[0] + unitDist;
-			newXcoords[2] = newXcoords[1];
-			newXcoords[3] = newXcoords[2] + unitDist;
-			newXcoords[4] = newXcoords[3];
-			
-			newYcoords[1] = yCoords[0];
-			newYcoords[2] = newYcoords[1] + unitDist;
-			newYcoords[3] = newYcoords[2];
-			newYcoords[4] = newYcoords[1];
-		}
+		lineLength = xCoords[0] - xCoords[1];
+		unitDist = lineLength / 3;
+		
+		newXcoords[1] = xCoords[0] - unitDist;
+		newXcoords[2] = xCoords[0] - unitDist;
+		newXcoords[3] = xCoords[0] - (unitDist*2);
+		newXcoords[4] = xCoords[0] - (unitDist*2);
+		
+		newYcoords[1] = yCoords[0];
+		newYcoords[2] = yCoords[0] - unitDist;
+		newYcoords[3] = yCoords[0] - unitDist;
+		newYcoords[4] = yCoords[0];
 	}
-
+	 
 	return ([newXcoords,newYcoords]);
-	
 }
